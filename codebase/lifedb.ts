de2e1c27b1b2f6d14564cd8f70bfd3a726d164b7
Sahasrangshu Guha <sguha1988.life@gmail.com>
2017-05-19 06:15:14 +0000

    work goes on

--- codebase/lifedb.ts
+++ codebase/lifedb.ts
@@ -1,3 +1,25 @@
 class LifeDB {
-    // This is the main class for life db
+    
+    private databaseName : string;
+    private errorMessage : object;
+    
+    /**
+     * @description- This is the constructor of the main class
+     * @params databaseName {String} - The name of the database
+     * @return {Null}
+     */
+    constructor (public userGivenDatabaseName:string) {
+        this.databaseName = userGivenDatabaseName;
+    }
+    
+    get databaseName() {
+        return this.databaseName;
+    }
+    
+    set databaseName(userGivenDatabaseName) {
+        if(this.databaseName.trim() !== "") {
+            this.warning(this.errorMessage.databaseChanged);
+        }
+        this.databaseName = userGivenDatabaseName;
+    }
 }
--- output/lifedb.js
+++ output/lifedb.js
@@ -1,5 +1,12 @@
 var LifeDB = (function () {
-    function LifeDB() {
+    /**
+     * @description- This is the constructor of the main class
+     * @params databaseName {String} - The name of the database
+     * @return {Null}
+     */
+    function LifeDB(userGivenDatabaseName) {
+        this.userGivenDatabaseName = userGivenDatabaseName;
+        this.databaseName = userGivenDatabaseName;
     }
     return LifeDB;
 }());
